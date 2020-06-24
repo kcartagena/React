27e272dbe9107d78e8ed2e6503439d589934dfe1
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-24 16:29:22 -0500

    expense-tracker

---
+++ expense-tracker-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Balance />
+
+
+    </div>
+  );
+}
+
+export default App;
