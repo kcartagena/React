4fcb1e399aef34b2b92a85d3e8f451a4ee8db61b
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-25 10:01:16 -0500

    delete action added

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -18,10 +18,21 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch ] = useReducer(AppReducer, initialState);
 
+    //actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (
     <GlobalContext.Provider 
-        value={{transactions:state.transactions}}
-        >
+        value={{
+            transactions:state.transactions,
+            deleteTransaction
+        
+        }}>
         {children}
     </GlobalContext.Provider>)
 }
