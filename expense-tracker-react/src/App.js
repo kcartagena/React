19d223134a08387c883be3b8cd4fb3ddf13cc846
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-25 09:33:36 -0500

    transaction components

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -5,9 +5,11 @@ import { Balance } from './components/Balance'
 import {IncomeExpenses} from './components/IncomeExpenses'
 import {TransactionList} from './components/TransactionList'
 import {AddTransaction} from './components/AddTransaction'
+import {GlobalProvider} from './context/GlobalState'
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
 
       <div className="container">
@@ -18,7 +20,7 @@ function App() {
         <AddTransaction />
       </div>
       
-    </div>
+    </GlobalProvider>
   );
 }
 
